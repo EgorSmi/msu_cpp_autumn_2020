164e786c235abcd976e759c6ff550693b7dafed7
Egor <Smirnov.egor.gena@mail.ru>
2020-12-02 03:48:38 +0300

    the first attempt

--- 08/main.cpp
+++ 08/main.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
 #include "ThreadPool.hpp"
 
-void foo()
-{
-    cout<<"1"<<endl;
-}
 
 void DefaultWorkTest()
 {
+    ThreadPool pool = ThreadPool(8);
+    auto task = pool.exec([]()
+    {
+        return 1;
+    });
+    if (task.get() == 1)
+    {
+        cout<<"OK! Default work test"<<endl;
+    }
+    else
+    {
+        cout<<"ERROR! Default work test"<<endl;
+    }
+}
+
+void TwoThreadsTest()
+{
     ThreadPool pool = ThreadPool(2);
-    auto task = pool.exec(foo);
-    task.get();
+    auto task1 = pool.exec([]()
+    {
+        return 1;
+    });
+    auto task2 = pool.exec([]()
+    {
+        return 2;
+    });
+    if ((task1.get() == 1) && (task2.get() == 2))
+    {
+        cout<<"OK! Two threads test"<<endl;
+    }
+    else
+    {
+        cout<<"ERROR! Incorrect work"<<endl;
+    }
 }
 
 int main()
 {
     DefaultWorkTest();
+    TwoThreadsTest();
     return 0;
 }
